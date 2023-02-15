3250b2c97588c1034dd56795ba1c8679bb7287a6
pt807 <pt807@naver.com>
2023-02-15 15:55:04 +0900

    springboot-myweb #1

---
+++ springboot-myapplication/src/main/java/ex05/MyAppplication.java
@@ -0,0 +1,20 @@
+package ex05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+/**
+ *
+ * Application Context가 구성된 이 후, 실행할 코드(Application Context 환경에 의존)가 있는 경우,
+ * ApplicationRunner 인터페이스의 구현 클래스 빈(HelloWorldRunner) 사용하기
+ *
+ */
+@SpringBootApplication
+public class MyAppplication {
+	public static void main(String[] args) {
+		try (ConfigurableApplicationContext ac = SpringApplication.run(MyAppplication.class, args)) {
+		}
+	}
+
+}
