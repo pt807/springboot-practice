6bdc8fb45c348c1c3e38838d8faf2252acae862a
pt807 <pt807@naver.com>
2023-02-16 09:56:47 +0900

    myweb #2

--- springboot-myweb/src/main/java/myweb/MyWebApplication.java
+++ springboot-myweb/src/main/java/myweb/MyWebApplication.java
@@ -4,17 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class MyWebApplication {
 
 	@Controller
 	public class HelloController {
-		@ResponseBody
 		@RequestMapping("hello")
 		public String hello() {
-			return "hello world";
+			return "/WEB-INF/views/hello.jsp";
 		}
 	}
 
