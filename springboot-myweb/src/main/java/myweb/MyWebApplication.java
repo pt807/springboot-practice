47c1d8e7e6cd140a0098ea3e013064489f8626a7
pt807 <pt807@naver.com>
2023-02-16 16:34:50 +0900

    properties 수정

--- springboot-myweb/src/main/java/myweb/MyWebApplication.java
+++ springboot-myweb/src/main/java/myweb/MyWebApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @SpringBootApplication
 public class MyWebApplication {
 
 	@Controller
 	public class HelloController {
+		@ResponseBody
 		@RequestMapping("/hello")
 		public String hello() {
 			return "hello";
